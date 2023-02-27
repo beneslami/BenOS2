9ee39297c3ddf8790d1028da1934ab47cd201ab3
ben <beneslami72@gmail.com>
2023-02-27 17:16:42 +0100

    major bug fix (NIC works)

--- src/hardwarecommunication/pci.cpp
+++ src/hardwarecommunication/pci.cpp
@@ -22,14 +22,14 @@ PeripheralComponentInterconnectController::~PeripheralComponentInterconnectContr
 }
 
 uint32_t PeripheralComponentInterconnectController::Read(uint16_t bus, uint16_t device, uint16_t function, uint32_t registeroffset){
-    uint32_t id = 0x1 << 31 | ((bus & 0xFF) << 16) | ((device & 0x1F) << 16) | ((function & 0x07) << 8) | (registeroffset & 0xFC);
+    uint32_t id = 0x1 << 31 | ((bus & 0xFF) << 16) | ((device & 0x1F) << 11) | ((function & 0x07) << 8) | (registeroffset & 0xFC);
     commandPort.Write(id);
     uint32_t result = dataPort.Read();
     return result >> (8*(registeroffset % 4));
 }
 
 void PeripheralComponentInterconnectController::Write(uint16_t bus, uint16_t device, uint16_t function, uint32_t registeroffset, uint32_t value){
-    uint32_t id = 0x1 << 31 | ((bus & 0xFF) << 16) | ((device & 0x1F) << 16) | ((function & 0x07) << 8) | (registeroffset & 0xFC);
+    uint32_t id = 0x1 << 31 | ((bus & 0xFF) << 16) | ((device & 0x1F) << 11) | ((function & 0x07) << 8) | (registeroffset & 0xFC);
     commandPort.Write(id);
     dataPort.Write(value);
 }
